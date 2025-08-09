9c063f92d171a03544cd3cf6775a1a7e77bf817f
linktoahmad <linktoahmedkhalil@gmail.com>
2025-08-09 13:43:38 +0100

    added stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+  anchorPlacement: "top-left",
+  duration: 1000,
 });
 
 // Add your javascript here
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      // Ensure it's an element node
+      if (node.tagName === "IFRAME" && node.getAttribute("height") === "30px") {
+        node.remove();
+      }
+    });
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+  console.log("MutationObserver started");
+});
